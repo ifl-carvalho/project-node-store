6974b6c88cfb4c3ad3104a1bf622255ba9ed8c21
ifaithfrost <ifaithfrost@gmail.com>
2021-04-04 01:29:17 -0300

    fix categories_view

--- src/views/products_view.ts
+++ src/views/products_view.ts
@@ -1,6 +1,6 @@
 import Product from '../models/Product';
 import imagesView from './images_view';
-import tagsView from './tags_view';
+import categoriesView from './categories_view';
 
 export default {
   render(product: Product) {
@@ -13,7 +13,7 @@ export default {
       title: product.title,
       description: product.description,
       images: imagesView.renderMany(product.images),
-      tags: tagsView.renderMany(product.categories)
+      categories: categoriesView.renderMany(product.categories)
     }
   },
 
